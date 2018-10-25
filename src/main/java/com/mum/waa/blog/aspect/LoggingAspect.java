9e6532395f693a271d0cc200e06af96412f647fa
yahia-elakeed <yahia.elakeed@gmail.com>
2018-10-24 22:38:12 -0500

    Add AspectJ

---
+++ src/main/java/com/mum/waa/blog/aspect/LoggingAspect.java
@@ -0,0 +1,31 @@
+package com.mum.waa.blog.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 
+ * @author Yahia
+ *
+ */
+@Aspect
+public class LoggingAspect {
+
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Before("execution(* com.mum.waa.blog.service.*.*(..))")
+	public void beforeServiceMethodsCall(JoinPoint joinpoint) {
+		logger.info(new StringBuilder().append("Before calling method : ").append(joinpoint.getSignature().getName())
+				.append(" in service layer").toString());
+	}
+
+	@After("execution(* com.mum.waa.blog.service.*.*(..))")
+	public void afterServiceMethodsCall(JoinPoint joinpoint) {
+		logger.info(new StringBuilder().append("After calling method : ").append(joinpoint.getSignature().getName())
+				.append(" in service layer").toString());
+	}
+}
